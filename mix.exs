1bb98fd83db0b171f74d9c5c918e29182c9c95fa
Gautham Ramachandran <gautham2r@gmail.com>
2018-02-18 16:54:07 +0530

    update doc config in mix

--- mix.exs
+++ mix.exs
@@ -1,37 +1,53 @@
 defmodule ServerTiming.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :server_timing,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: description(),
       deps: deps(),
       package: package(),
       name: "server_timing",
+      docs: docs_config(),
       source_url: "https://github.com/deftia/server_timing"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug, "~> 1.4"}
+      {:plug, "~> 1.4"},
+      {:ex_doc, "~> 0.13", only: :dev},
     ]
+  end
+
+  defp description do
+    """
+    Simple Plug for Server Timing
+    """
+  end
+
+  defp docs_config do
+    [extras: ["README.md": [title: "Overview", path: "overview"]],
+     main: "overview",
+     source_ref: "v#{@version}",
+     source_url: "https://github.com/deftia/server_timing"]
   end
 
   defp package do
     [
       name: "server_timing",
-      files: ["lib", "priv", "mix.exs", "README*", "readme", "LICENSE*", "license*"],
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
       maintainers: ["Abdulsattar Mohammed", "Gautham Ramachandran"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/deftia/server_timing"}
