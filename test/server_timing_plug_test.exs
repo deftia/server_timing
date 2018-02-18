62d91bd1d7475e859b37f922095b6f6e7f6a1176
Gautham Ramachandran <gautham2r@gmail.com>
2018-02-18 16:36:37 +0530

    update plug tests.

--- test/server_timing_plug_test.exs
+++ test/server_timing_plug_test.exs
@@ -1,4 +1,4 @@
-defmodule ServerTimingTest do
+defmodule ServerTimingPlugTest do
   use ExUnit.Case
   use Plug.Test
 
@@ -9,7 +9,7 @@ defmodule ServerTimingTest do
 
   test "set start time on conn", context do
     conn = ServerTiming.start(context[:conn], :test)
-    assert Map.has_key?(conn.assigns[:server_timing], :test) == true
+    assert Keyword.has_key?(conn.assigns[:server_timing], :test) == true
     assert conn.assigns[:server_timing][:test] != nil
     assert conn.assigns[:server_timing][:test].start_time != nil
     assert conn.assigns[:server_timing][:test].end_time == nil
@@ -18,9 +18,20 @@ defmodule ServerTimingTest do
   test "stop time must update end_time", context do
     conn = ServerTiming.start(context[:conn], :test)
     conn = ServerTiming.stop(conn, :test)
-    assert Map.has_key?(conn.assigns[:server_timing], :test) == true
+    assert Keyword.has_key?(conn.assigns[:server_timing], :test) == true
     assert conn.assigns[:server_timing][:test] != nil
     assert conn.assigns[:server_timing][:test].start_time != nil
     assert conn.assigns[:server_timing][:test].end_time != nil
+  end
+
+  test "set server-timing header with each timing info", context do
+    conn = ServerTiming.start(context[:conn], :test)
+    conn = ServerTiming.stop(conn, :test)
+    resp_conn = send_resp(conn, 200, "")
+    sent_conn = sent_resp(resp_conn)
+    headers = elem(sent_conn, 1)
+    assert Enum.empty?(headers) == false
+    server_timings = Enum.find(headers, fn(x) -> elem(x, 0) == "server-timing" end) 
+    assert server_timings != nil
   end
 end
